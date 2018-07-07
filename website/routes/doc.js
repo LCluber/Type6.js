6ae3d27b022e81df460f38c2cdfed3d36f41683d
lcluber <ludovic.cluber@gmail.com>
2018-07-07 22:38:44 +0200

    fix documentation route

--- website/routes/doc.js
+++ website/routes/doc.js
@@ -2,7 +2,6 @@ var express = require('express');
 var path    = require('path');
 var app     = express();
 
-app.use(express.static('doc'));
-//app.use('/', express.static(path.join(__dirname, '../../doc')));
+app.use('/', express.static(path.join(__dirname, '../../doc')));
 
 module.exports = app;
