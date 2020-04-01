36778bd875028acd8501a467afa695ddf5dceb6b
Estelle <est.picq@gmail.com>
2020-04-01 11:49:48 +0200

    side menu design

--- web/js/docs.js
+++ web/js/docs.js
@@ -1,59 +1,17 @@
+$(document).ready(function () {
 
-
-$(document).ready(function() {
-	
 	/* ====== Toggle Sidebar ======= */
-	
-	$('#sidebarToggler').on('click', function() {
-	
-		if ( $('#docs-sidebar').hasClass('sidebar-visible') ) {
-
-			  $("#docs-sidebar").removeClass('sidebar-visible').addClass('sidebar-hidden');
-			
+	$('#sidebarToggler').on('click', function () {
+		if ($('#docs-sidebar').hasClass('sidebar-visible')) {
+			$("#docs-sidebar").removeClass('sidebar-visible').addClass('sidebar-hidden');
 		} else {
-
-			  $("#docs-sidebar").removeClass('sidebar-hidden').addClass('sidebar-visible');
-			
+			$("#docs-sidebar").removeClass('sidebar-hidden').addClass('sidebar-visible');
 		}
-			
-    });
-    
+	});
 
-    /* ====== Activate scrollspy menu ===== */
-    // $('body').scrollspy({target: '#docs-nav', offset: 100});
-    
-    
-    
-    /* ===== Smooth scrolling ====== */
-	// $('#docs-sidebar a.scrollto').on('click', function(e){
-    //     //store hash
-    //     var target = this.hash;    
-    //     e.preventDefault();
-	// 	$('body').scrollTo(target, 800, {offset: -69, 'axis':'y'});
-		
-	// 	//Collapse sidebar after clicking
-	// 	if ($('#docs-sidebar').hasClass('sidebar-visible') && $(window).width() < 1200){
-	// 		$('#docs-sidebar').removeClass('sidebar-visible').addClass('slidebar-hidden');
-	// 	}
-		
+	// $(document).delegate('*[data-toggle="lightbox"]', 'click', function (e) {
+	// 	e.preventDefault();
+	// 	$(this).ekkoLightbox();
 	// });
-	
-	/* wmooth scrolling on page load if URL has a hash */
-	// if(window.location.hash) {
-	// 	var urlhash = window.location.hash;
-	// 	$('body').scrollTo(urlhash, 800, {offset: -69, 'axis':'y'});
-	// }
-	
-	
-	/* Bootstrap lightbox */
-    /* Ref: http://ashleydw.github.io/lightbox/ */
-
-    $(document).delegate('*[data-toggle="lightbox"]', 'click', function(e) {
-        e.preventDefault();
-        $(this).ekkoLightbox();
-    }); 
-
-    
-    
 
 });
